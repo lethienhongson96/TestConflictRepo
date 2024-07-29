cc9098d108f9ef4d0f6564260d570563af6ece40
Amberos - Son Le <son.le@amberos.com>
2024-07-30 00:34:34 +0700

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//line 1
+//line 2
+//line 3
+//line 4
+//line 5
+//line 6
+//line 7
+//line 8
